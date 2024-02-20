bbf4a16751191254dd826b06108c133f6dcd9c2a
ryan01131496 <cuklivsky@gmail.com>
2024-02-20 21:15:27 +0800

    Initialize the spring boot project

---
+++ RyanShoppingMall/src/main/java/com/ryan/ryanshoppingmall/RyanShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.ryanshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanShoppingMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyanShoppingMallApplication.class, args);
+    }
+
+}
---
+++ RyanShoppingMall/src/test/java/com/ryan/ryanshoppingmall/RyanShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.ryanshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
